0c345d7f8ee3bd89525955420af4a3299c4c568d
big-lanpa <153191398+big-lanpa@users.noreply.github.com>
2024-01-18 16:07:55 +0800

    完整程式碼
    七段功能

--- ballgame.vhd
+++ ballgame.vhd
@@ -7,6 +7,7 @@ use ieee.numeric_std.all;
 entity ballgame is
 	port(	clk,reset	  :in std_logic;
 			btn1,btn2,btn3:in std_logic;
+			x1,x2         :out std_logic_vector(6 downto 0);
 			q			  :out std_logic_vector(7 downto 0)
 		);
 end ballgame;
@@ -18,8 +19,8 @@ architecture Behavioral of ballgame is
 	signal in1  :integer range 0 to 1;
 	signal state:integer range 0 to 16;
 	
-	signal play1:integer range 0 to 2;
-	signal play2:integer range 0 to 2;
+	signal play1:std_logic_vector(3 downto 0);
+	signal play2:std_logic_vector(3 downto 0);
 	
 	signal ball :std_logic_vector(7 downto 0);
 	
@@ -33,11 +34,12 @@ begin
 		End if;
 	end process;
 	fc<=div(24);
+	
 	process(fc)
 	begin
 		if rising_edge(fc) then
 			case state is
-				when 16=>
+				when 16=>                  --初始狀態
 					ball<="00000000";
 					if btn1='1' then
 						state<=0;
@@ -46,12 +48,12 @@ begin
 					end if;
 					
 				when 0=>
-					ball<="00000000";
+					ball<="00000000";     --從左邊發球
 					if btn1='1' then
 						state<=1;
 					end if;
 					
-				when 15=>
+				when 15=>                 --從右邊發球
 					if btn2='1' then
 						in1<=1;
 						ball<="10000000";
@@ -146,7 +148,7 @@ begin
 				when 10=>
 					ball<="00000000";
 					play1<=play1+1;
-					if play1=2 then
+					if play1="0011" then
 						state<=12;
 					else state<=0;
 					end if;
@@ -154,7 +156,7 @@ begin
 				when 11=>
 					ball<="00000000";
 					play2<=play2+1;
-					if play2=2 then
+					if play2="0011" then
 						state<=13;
 					else state<=15;
 					end if;
@@ -172,14 +174,40 @@ begin
 					end if;
 					
 				when 14=>
-					play1<=0;
-					play2<=0;
+					play1<="0000";
+					play2<="0000";
 					state<=16;
 				
 
 			end case;
 		end if;
 	end process;
-			
+	
+
 	q<=ball;
+	with play1 select
+		x1<="1111110" when "0000",
+			"0110000" when "0001",
+			"1101101" when "0010",
+			"1111001" when "0011",
+			"1111001" when "0100",
+			"1011011" when "0101",
+			"1011111" when "0110",
+			"1110000" when "0111",
+			"1111111" when "1000",
+			"1111011" when "1001",
+			"0000000" when others;
+	with play2 select
+		x2<="1111110" when "0000",
+			"0110000" when "0001",
+			"1101101" when "0010",
+			"1111001" when "0011",
+			"1111001" when "0100",
+			"1011011" when "0101",
+			"1011111" when "0110",
+			"1110000" when "0111",
+			"1111111" when "1000",
+			"1111011" when "1001",
+			"0000000" when others;
+
 end Behavioral;
